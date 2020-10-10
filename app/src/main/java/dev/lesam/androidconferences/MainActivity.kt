48d2822040174eb1572fcf96e1e77892eebda401
Oussama HAFFERSSAS <hfrs.oussama@gmail.com>
2020-10-10 22:24:36 +0200

    Initial commit

---
+++ app/src/main/java/dev/lesam/androidconferences/MainActivity.kt
@@ -0,0 +1,49 @@
+package dev.lesam.androidconferences
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import dev.lesam.androidconferences.ui.AndroidConferencesTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidConferencesTheme {
+                Scaffold(
+                        topBar = { 
+                            TopAppBar(
+                                    title = { Text(text = "Android Conferences") },
+                                    backgroundColor = MaterialTheme.colors.primary
+                            )
+                        }
+                ) {
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background) {
+                        Greeting("Android")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidConferencesTheme {
+        Greeting("Android")
+    }
+}
