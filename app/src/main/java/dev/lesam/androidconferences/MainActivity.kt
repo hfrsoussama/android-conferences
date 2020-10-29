07ed8d36f0728c7b130c28425ff492058312f155
Oussama Hafferssas <hfrs.oussama@gmail.com>
2020-10-29 20:26:01 +0100

    feat(baby-step) : Upgraded Compose alpha06

--- app/src/main/java/dev/lesam/androidconferences/MainActivity.kt
+++ app/src/main/java/dev/lesam/androidconferences/MainActivity.kt
@@ -2,7 +2,6 @@ package dev.lesam.androidconferences
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
